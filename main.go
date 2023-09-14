f6c7d5e3f8cd908742daa0f6bac09a1c38393c07
lwileczek <pleazenospam@protonmail.com>
2023-09-14 16:57:27 -0400

    let users cap the number of results

--- main.go
+++ main.go
@@ -19,13 +19,14 @@ var IGNORE_PATHS = [7]string{
 }
 
 func main() {
-    //The number of workers. If there are more workers the system can read from
-    //the work queue more often and a larger queue is not required. It's a blance
-	jobs := flag.Int("j", 4, "Estimated number of workers?")
-    //If this is reached the system could end up in deadlock
-    //The bigger the queue size the more memory is used
-    //Smaller could be faster but you coud have deadlock
-	queueSize := flag.Int("q", 128, "The max work queue size")
+	//The number of workers. If there are more workers the system can read from
+	//the work queue more often and a larger queue is not required. It's a blance
+	workers := flag.Int("w", 8, "Number of workers")
+	//If this is reached the system could end up in deadlock
+	//The bigger the queue size the more memory is used
+	//Smaller could be faster but you coud have deadlock
+	queueSize := flag.Int("q", 2048, "The max work queue size")
+	maxResults := flag.Int("c", -1, "The maximum number of results to find")
 	dir := flag.String("d", ".", "The starting directory to check for files")
 	pattern := flag.String("p", "", "A pattern to check for within the file names")
 	flag.Parse()
@@ -41,21 +42,36 @@ func main() {
 		*dir = string((*dir)[0 : len(*dir)-1])
 	}
 
-	printCh := make(chan string, *jobs)
-    //The system will reach deadlock if the work queue reaches capacity
+	printCh := make(chan string, *workers)
+	//The system will reach deadlock if the work queue reaches capacity
 	workQ := make(chan string, *queueSize)
 	dirCount := make(chan int)
 	//Track how many dirs are open and close the work queue when we hit zero
 	go dirChecker(dirCount, workQ)
 	defer close(dirCount)
-	go createWorkerPool(pattern, workQ, printCh, dirCount, jobs)
+	go createWorkerPool(pattern, workQ, printCh, dirCount, workers)
 
 	//Send first work request
 	workQ <- *dir
 
 	//Print all results
-	for item := range printCh {
-		fmt.Println(item)
+	showResults(printCh, maxResults)
+}
+
+func showResults(ch chan string, limit *int) {
+	if *limit > 0 {
+		n := 0
+		for item := range ch {
+			fmt.Println(item)
+			n++
+			if n >= *limit {
+				return
+			}
+		}
+	} else {
+		for item := range ch {
+			fmt.Println(item)
+		}
 	}
 }
 
@@ -99,9 +115,9 @@ func search(pattern *string, in chan string, out chan string, cnt chan int) {
 						continue ItemSearch
 					}
 				}
-                subPath := fmt.Sprintf("%s/%s", path, item.Name())
+				subPath := fmt.Sprintf("%s/%s", path, item.Name())
 				cnt <- 1
-                in <- subPath
+				in <- subPath
 			} else {
 				if strings.Index(item.Name(), *pattern) >= 0 {
 					//subPath is repeated but no point in creating an allocation if not required
